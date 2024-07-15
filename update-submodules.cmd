820aea65bdcec4397be71e10c0a8dfd85d60f008
JustAnotherID <just-another-id@outlook.com>
2024-07-16 01:29:22 +0800

    chore: bump submodules
    Signed-off-by: JustAnotherID <just-another-id@outlook.com>

--- update-submodules.cmd
+++ update-submodules.cmd
@@ -2,6 +2,13 @@
 git pull
 git submodule init
 git submodule update
-git submodule foreach "git fetch origin --prune & git reset --hard origin/master"
+
+git submodule foreach "git fetch origin --prune"
+cd go-cqhttp & git reset --hard origin/master & cd ..
+cd sealdice-android & git reset --hard origin/master & cd ..
+cd sealdice-builtins & git reset --hard origin/master & cd ..
+cd sealdice-core & git reset --hard origin/main & cd ..
+cd sealdice-ui & git reset --hard origin/master & cd ..
+
 git commit -am "chore: bump submodules"
 git push
