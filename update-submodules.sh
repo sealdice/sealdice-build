52679e087432ce020985a1171802848d0bbd24ce
fy <fy0748@gmail.com>
2024-08-08 12:49:35 +0000

    feat: 1.4.6 release

--- update-submodules.sh
+++ update-submodules.sh
@@ -4,5 +4,4 @@ git submodule update
 cd sealdice-builtins && git pull origin master && cd ..
 cd sealdice-ui && git pull origin master && cd ..
 cd sealdice-android && git pull origin master && cd ..
-cd go-cqhttp && git pull origin master && cd ..
 cd sealdice-core && git pull origin master && cd ..
